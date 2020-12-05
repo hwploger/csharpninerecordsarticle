ee1916b8c346eaa4040b3f3b910bc0467aa48ebc
Will Ploger <william.ploger@doubleksoftware.com>
2020-12-05 13:04:59 -0600

    Init Commit of Solution and Project

---
+++ csharpninerecordsarticle/csharpninerecordsarticle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharpninerecordsarticle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
